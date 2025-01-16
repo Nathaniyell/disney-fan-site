251f74a5affea99efd8c1c2250e4ce772bec2130
Nathaniyell <essien.nathan@yahoo.com>
2025-01-16 22:06:21 +0100

    Add header to layout

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,18 @@
+"use client"
 import { Input } from "@/components/ui/input"
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import { UserCircle } from 'lucide-react'
 import Image from "next/image"
 import Link from "next/link"
 
-export function SiteHeader() {
+
+export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-[#051138]">
+    <header className="sticky top-0 z-50 w-full border-b">
       <div className="container flex h-16 items-center px-4 sm:px-8">
         <Link href="/" className="mr-8">
           <Image
-            src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-zZ5xkWXpYdQ4DRpYj2qEJqzWVX66U5.png"
+            src="/public/logo.png"
             alt="Disney"
             width={80}
             height={40}
@@ -24,7 +26,7 @@ export function SiteHeader() {
               <Input
                 type="search"
                 placeholder="Find a character..."
-                className="h-9 w-full bg-white/95 pr-10 placeholder:text-gray-500 dark:bg-white/95 dark:placeholder:text-gray-500"
+                className="h-9 w-full bg-white/95 pr-10 placeholder:text-gray-500 dark:bg-white/95 dark:placeholder:text-gray-500 rounded-full"
               />
             </div>
           </div>
